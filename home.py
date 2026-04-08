f862bf965930a735ea8a4f5a1df6bd8270206299
Harshit Chaurasia <harshitchaurasia153@gmail.com>
2026-04-08 15:46:45 +0530

    Add files via upload

---
+++ home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+about_me=st.Page(
+    page='views\\about.py',
+    title='About us',
+    icon=':material/home:',
+    default=True
+)
+contact_me=st.Page(
+    page='views\\contact.py',
+    title='Contact us',
+    icon=':material/contact_mail:',
+)
+service_me=st.Page(
+    page='views\\services.py',
+    title='Services',
+    icon=':material/engineering:',
+)
+pg=st.navigation(pages=[about_me,contact_me,service_me])
+# st.logo('apply')
+pg.run()
