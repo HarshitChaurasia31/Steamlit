61d6a4c330966de90ee67ebc09844078eb3db227
Radaxo31 <harshitchaurasia153@gmail.com>
2026-04-08 16:02:14 +0530

    Machine sutiable

--- home.py
+++ home.py
@@ -1,17 +1,17 @@
 import streamlit as st
 about_me=st.Page(
-    page='views\\about.py',
+    page='views/about.py',
     title='About us',
     icon=':material/home:',
     default=True
 )
 contact_me=st.Page(
-    page='views\\contact.py',
+    page='views/contact.py',
     title='Contact us',
     icon=':material/contact_mail:',
 )
 service_me=st.Page(
-    page='views\\services.py',
+    page='views/services.py',
     title='Services',
     icon=':material/engineering:',
 )
